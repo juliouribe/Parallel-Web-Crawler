52f3aa12124ed638fd20cae333c018b18d1961ec
Julio C. Uribe <uribejulioc@gmail.com>
2023-02-01 22:16:08 -0800

    Implement write methods for CrawlerResultWriter

--- webcrawler/src/main/java/com/udacity/webcrawler/json/CrawlResultWriter.java
+++ webcrawler/src/main/java/com/udacity/webcrawler/json/CrawlResultWriter.java
@@ -1,6 +1,11 @@
 package com.udacity.webcrawler.json;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
 import java.io.Writer;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Objects;
 
@@ -28,7 +33,11 @@ public final class CrawlResultWriter {
   public void write(Path path) {
     // This is here to get rid of the unused variable warning.
     Objects.requireNonNull(path);
-    // TODO: Fill in this method.
+    try (Writer writer = Files.newBufferedWriter(path)) {
+      write(writer);
+    } catch (IOException ex) {
+      System.out.println(ex.getLocalizedMessage());
+    }
   }
 
   /**
@@ -39,6 +48,12 @@ public final class CrawlResultWriter {
   public void write(Writer writer) {
     // This is here to get rid of the unused variable warning.
     Objects.requireNonNull(writer);
-    // TODO: Fill in this method.
+    ObjectMapper objectMapper = new ObjectMapper();
+    objectMapper.disable(JsonGenerator.Feature.AUTO_CLOSE_TARGET);
+    try {
+      objectMapper.writeValue(writer, result);
+    } catch (IOException e) {
+      System.out.println(e.getLocalizedMessage());
+    }
   }
 }
